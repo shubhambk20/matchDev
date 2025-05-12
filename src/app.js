17f507a4ce10901b1a3545933d1e3a3b5c64be3e
sohambalekar <soham@webarcitech.com>
2025-05-12 15:16:42 +0530

    created express server

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express() 
+const port = 3000
+
+app.use("/home", (req, res) => {
+    res.send("This is sweet home...")
+})
+
+app.use("/hello", (req, res) => {
+    res.send("Hello, Who's there :-O ");
+})
+
+app.use((req, res) => {
+    res.send("This is a wildcard route ...")
+})
+
+app.listen(port, (req, res) => {
+    console.log(`App listending on ${port}`)
+});
