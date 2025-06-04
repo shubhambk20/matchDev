8ed71f967fb7c6635b51a1385219853e0c498c52
Shubham bk <shubhambk20@gmail.com>
2025-06-04 08:10:37 +0530

    get APIs

--- src/app.js
+++ src/app.js
@@ -17,6 +17,33 @@ app.post("/signup", async (req, res) => {
     }
 })
 
+// Get User by email 
+app.get('/user', async (req, res) => {
+    const emailId = req.body.emailId 
+
+    try {
+        const user = await User.findOne()
+
+        if (!user) {
+            res.status(404).send("User not found.")
+        } else {
+            res.send(user)
+        }
+    } catch (error) {
+        res.status(400).send("Something went wrong.")
+    }
+})
+
+// Feed API - get the all the users from database
+app.get('/feed', async (req, res) => {
+    try {
+        const users = await User.find({})
+        res.send(users)
+    } catch(Error) {
+        res.status(400).send("Something went wrong.")
+    }
+})
+
 connectDB()
     .then(() => {
         console.log("Database connection established...");
